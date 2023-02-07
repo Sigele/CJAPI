3e5732a49ae61210cd76d3b8e3f6f44a3c1ad6d0
Sigele <sigeleakosua@gmail.com>
2023-02-07 00:02:20 -0600

    building boilerplate for scraping func. Still not linked to page

--- wikiscraper/scraper.py
+++ wikiscraper/scraper.py
@@ -14,3 +14,17 @@ with open('test.html', 'r') as html_file:
   for tag in tags:
     print(tag.text)
   print(tags)
+
+  #within table (tbody):
+  #tr, td, b displays radical input
+  #tr, td (2nd) contains QWERTY input
+  #tr, td (3rd), span, a "title" gives char output
+
+  #trs dont have any class or other atributes...this is the best I can do
+  #to find by class: class_="examplestring" as 2nd arg
+  entries = soup.find_all('tr')
+  for entry in entries: 
+    rad_input = entry.td.b
+    #not sure about this one...still no classes
+    qwerty_input = entry.td.text
+    characters = entry.td.span.a.text
