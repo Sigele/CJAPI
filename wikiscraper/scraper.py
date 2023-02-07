f43488431497bce0d89d77787e721c1df1fe4bba
Sigele <sigeleakosua@gmail.com>
2023-02-06 23:43:18 -0600

    installed test libraries, created BS instance

--- wikiscraper/scraper.py
+++ wikiscraper/scraper.py
@@ -5,3 +5,12 @@ from bs4 import BeautifulSoup
 with open('test.html', 'r') as html_file:
   content = html_file.read()
   print(content)
+#create BS instance
+  soup = BeautifulSoup(content, 'lxml')
+  
+  #find_all returns list of all matching elements
+  tags = soup.find_all('text')
+
+  for tag in tags:
+    print(tag.text)
+  print(tags)
