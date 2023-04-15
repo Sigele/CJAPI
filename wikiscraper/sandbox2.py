d3b709b946d43ff4800ef74c58bc7499b6941f86
Sigele <sigeleakosua@gmail.com>
2023-04-14 22:23:59 -0500

    cwf sandbox2. working on combine basic scrape and edge functions into a single file, but doubledEntry logic isn't working as intended. Just beginning to debug

--- wikiscraper/sandbox2.py
+++ wikiscraper/sandbox2.py
@@ -1,23 +1,79 @@
-#importing bs4, requests module, regex module
+# currently refactoring successful(?) scrape functionality...there's a bit to much unecessary space being taken up
 
 from bs4 import BeautifulSoup
 import requests
 import re
+from pprint import pprint as pp
 
-from radLinks import URLgrab
-from charGrab import charGrab
-#grab all URLS
+from entryClass import *
 
-radEntry = 'https://en.wiktionary.org/wiki/Appendix:Chinese_Cangjie'
+testURL = 'https://en.wiktionary.org/wiki/Appendix:Chinese_Cangjie/%E7%81%AB'
+
+hrefHead = 'https://en.wiktionary.org';
+html_text = requests.get(testURL).text
+
+soup = BeautifulSoup(html_text, 'lxml')
+table = soup.find_all('tr')
+
+# returns list of 3-element sublists. Each sublist represents an entry for a single character.
+
+def getEntries(url, table):
+    elements = []
+    for trow in table:
+      td = trow.find_all('td')
+      elements.append(td)
+
+    return elements
+
+
+def dblEntryCheck(three_tds):
+    #if the element that holds the a elements contains 2 different ones, we need to save the titles but give everything else identical attributes
+    result = []
+    #should the entry building happen here? or in the main function?
+    # uhhhh here to avoid redundancies in the main function
+    if len(three_tds[2].find(class_='Hani').text) > 2:
+      names = three_tds[2].find_all('a')
+      for name in names:
+         e2 = Entry()
+         e2.get_character(name.text)
+         e2.get_qwerty(three_tds[1].text[1:-1])
+         e2.get_radicals(three_tds[0].text)
+         e2.get_level(len(e2.radicals))
+         if(brokenLinkCheck(three_tds) == True):
+            e2.get_link('Sorry, ', 'the link is missing!')
+         else: 
+            e2.get_link(hrefHead, name['href'])
+         result.append(e2)   
+    if (len(result) == 0): return
+    return result;
+
+def brokenLinkCheck(listOf3Elements):
+  if len(listOf3Elements[2].find('a')['title']) > 1:
+    return True
+
+def populate(ele, list):
+
+  entry = Entry()
+  entry.get_character(ele[2].find(class_='Hani').text)
+  entry.get_qwerty(ele[1].text[1:-1])
+  entry.get_radicals(ele[0].text)
+  entry.get_level(len(entry.radicals))
+  if (brokenLinkCheck(ele) == True):
+    entry.get_link('Sorry, ' , 'the link is missing!')
+  else:
+    entry.get_link(hrefHead, ele[2].find('a')['href'])
+  list.append(entry)
+
+
+tds = getEntries(testURL,table)
+print(len(tds))
 data = []
+for ele in tds:
+   doubles = dblEntryCheck(ele)
+   if (type(doubles) == 'list'):
+      for dbl in doubles:
+         data.append(dbl)
+   else: 
+      populate(ele, data)
 
-links = URLgrab(radEntry)
-
-# DONE.
-for link in links:
-    data.append(charGrab(link));
-print(data)
-#for each URL, parse for characters
-
-#DONE, but the Cyrillic bug is back :( might have to write a custom func since it's an edge case
-
+pp(data[29].__dict__)
