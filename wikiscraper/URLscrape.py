359e7e6fee0ae42bba18aadd0def6bda30dfd885
Sigele <sigeleakosua@gmail.com>
2023-04-19 20:35:31 -0500

    cwf URLscrape: grabbed list of all radical urls, which is the starting point for the entire sccrape. Next step is to write tests for the file and test combining this functionality with indv entry grab

--- wikiscraper/URLscrape.py
+++ wikiscraper/URLscrape.py
@@ -2,13 +2,20 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
-urls = requests.get('https://en.wiktionary.org/wiki/Appendix:Chinese_Cangjie').text
+entryURL = 'https://en.wiktionary.org/wiki/Appendix:Chinese_Cangjie'
+urls = requests.get(entryURL).text
 
 
 # need to loop through all <a> elements within <span> with class Zyyyy
 
 url_soup = BeautifulSoup(urls, 'lxml')
 span = url_soup.find(class_="Zyyy")
-urls = span.contents
-print(urls)
-print(url_soup.find_all(href=re.compile('/wiki/Appendix:Chinese_Cangjie/')))
+urls = span.find_all('a')
+print(len(urls))
+data = []
+for url in urls:
+    data.append(entryURL + '/' + url.text)
+
+print(data)
+    
+# print(url_soup.find_all(href=re.compile('/wiki/Appendix:Chinese_Cangjie/')))
