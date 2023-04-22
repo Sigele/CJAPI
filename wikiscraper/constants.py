95ee4b313776718b4b8043b55c5d7b476b50b317
Sigele <sigeleakosua@gmail.com>
2023-04-21 22:03:01 -0500

    cwf constants: mapping radicals to qwerty for testing purposes, steadily building out entry class unittest suite

--- wikiscraper/constants.py
+++ wikiscraper/constants.py
@@ -4,3 +4,28 @@ import requests
 entryURL = 'https://en.wiktionary.org/wiki/Appendix:Chinese_Cangjie';
 
 hrefHead = 'https://en.wiktionary.org'
+
+def radMap(qwerty):
+  rads = {
+        "A":"日", "B":"月", "C":"金", "D":"木",
+        "E":"水", "F":"火", "G":"土", "H":"竹",
+        "I":"戈", "J":"十", "K":"大", "L":"中",
+        "M":"一", "N":"弓", "O":"人", "P":"心",
+        "Q":"手", "R":"口", "S":"尸", "T":"廿",
+        "U":"山", "V":"女", "W":"田", "X":"難",
+        "Y":"卜", "Z":"重"
+    }
+  
+
+  def get_key(val):
+    for key, value in rads.items():
+        if val == value:
+          print(key)
+          return key
+        else:
+           return value
+ 
+    return "key doesn't exist"
+
+
+radMap('女')
--- wikiscraper/test_entry.py
+++ wikiscraper/test_entry.py
@@ -3,6 +3,9 @@ import re
 from constants import *
 from entryClass import Entry
 
+
+
+
 class TestEntry(unittest.TestCase):
 
     def setUp(self):
@@ -26,8 +29,41 @@ class TestEntry(unittest.TestCase):
         self.e_1.get_qwerty('AWS')
         self.assertIsInstance(self.e_1.qwerty, str, '"qwerty" property should be type str')
         # ensure only A-Z characters
-        
-        # ensure length less than 5
+        self.assertTrue(re.compile(r'^[A-Z]+$').match(self.e_1.qwerty), 'str should contain only characters A-Z, case sensitive')
+        # ensure length less than 6
+        self.assertTrue(len(self.e_1.qwerty) < 6, 'qwerty input string should be 5 characters or less')
 
+    def test_radicals(self):
+        # radicals should be a list of single character strings
+        self.e_1.get_radicals(
+            [
+            "口",
+            "月",
+            "月",
+            "水"
+            ])
+        self.assertIsInstance(self.e_1.radicals, list, '"radicals" should be a list')
+        for char in self.e_1.radicals:
+            self.assertIsInstance(char, str)
+            self.assertTrue(len(char) == 1, 'each radical list element should be a single character')
+        # radicals should be CJK values only
+            self.assertTrue(re.search(u'[\u4e00-\u9fff]',char), 'each character in "radicals" should be CJK ideograph')
+        # radicals length should match qwerty length
+        self.e_1.get_qwerty("RBBE")
+        self.assertEqual(len(self.e_1.qwerty), len(self.e_1.radicals))
+        # radicals should map to qwerty exactly
+
+    # def test_link(self):
+    #     # all links should go to wiktionary
+    #     # link must not be broken
+    #     # should be a string
+
+    # def test_level(self):
+    #     # level should be a number
+    #     # level should equal length of qwerty and/or radicals
+
+    # def test_doubled(self):
+    #     # should be a boolean value
+    #     # cannot be null
 if __name__ == '__main__':
     unittest.main()
