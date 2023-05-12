2c5fe0be2024352da5b80f6c94fc44128a1e2966
Sigele <sigeleakosua@gmail.com>
2023-05-11 20:03:01 -0500

    It worked but...let that man cook. Might add logic to print the time this takes. I'm guessing about 300000ms

--- wikiscraper/fullScrape.py
+++ wikiscraper/fullScrape.py
@@ -19,31 +19,59 @@ from firstScrape import *
 #defined in firstScrape
 links = URLgrab(Entry.entryURL)
 
-#trying with smaller # first
-testLink = links[random.randrange(0,24,1)]
-print('testURL', testURL)
-
-testSoup = BeautifulSoup(requests.get(testLink).text, 'lxml')
-table = testSoup.findAll('tr')
-#???
-testLambda = []
-tds = getEntries(table)
-
+allTds = []
 dict = []
+for link in links:
+   linkSoup = BeautifulSoup(requests.get(link).text, 'lxml')
+   linkTable = linkSoup.findAll('tr')
+   linkTds = getEntries(linkTable)
+   
+   allTds.extend(linkTds)
+#should be 27043
+print(len(allTds))
 
-for ele in tds:
-    doubles = dblEntryCheck(ele)
-    if hasattr(doubles, '__len__'):
+for ele in allTds:
+   doubles = dblEntryCheck(ele)
+   if hasattr(doubles, '__len__'):
       # print('working')
       # pp(doubles[1].__dict__)
       for dbl in doubles:
          dict.append(dbl)   
-    else:
+   else:
       populate(ele,dict)
 
 testy = []
 
-for item in dict[:10]:
+for item in dict:
    testy.append(item.__dict__)
 with open('wikiscraper/writetest.json', 'w') as final:
    json.dump(testy, final, indent=4, ensure_ascii=False)
+
+#trying with smaller # first
+# testLink = links[random.randrange(0,24,1)]
+# print('testURL', testURL)
+
+# testSoup = BeautifulSoup(requests.get(testLink).text, 'lxml')
+# table = testSoup.findAll('tr')
+# #???
+# testLambda = []
+# tds = getEntries(table)
+
+# dict = []
+
+# for ele in tds:
+#     doubles = dblEntryCheck(ele)
+#     if hasattr(doubles, '__len__'):
+#       # print('working')
+#       # pp(doubles[1].__dict__)
+#       for dbl in doubles:
+#          dict.append(dbl)   
+#     else:
+#       populate(ele,dict)
+
+# testy = []
+
+# for item in dict[:10]:
+#    testy.append(item.__dict__)
+# with open('wikiscraper/writetest.json', 'w') as final:
+#    json.dump(testy, final, indent=4, ensure_ascii=False)
